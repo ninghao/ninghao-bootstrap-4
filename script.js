6457d4b6ae633a33caaedfc18cc1710a57d0692d
wanghao8080 <117663444@qq.com>
2017-10-29 15:04:07 +0800

    Alerts：可关闭的提示

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(() => {
 
-  
+  $('.close-alert').click(() => {
+    $('.alert').alert('close')
+  })
+
+  $('.alert').on('close.bs.alert', () => {
+    console.log('close alert')
+  })
+
+  $('.alert').on('closed.bs.alert', () => {
+    console.log('closed alert')
+  })
 
 })
