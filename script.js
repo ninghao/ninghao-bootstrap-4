2225ad5db7b45f128d4e84c4212ac40587416400
wanghao8080 <117663444@qq.com>
2017-10-30 14:22:11 +0800

    Carousel：配置选项，方法与事件

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(document).ready(() => {
 
-  
+  // $('.carousel').carousel({
+  //   // interval: 1000
+  // })
+
+  $('.carousel').on('slide.bs.carousel', (event) => {
+    console.log('slide: ', `
+      方向：${ event.direction }
+      从：${ event.from }
+      到：${ event.to }
+    `)
+  })
+  $('.carousel').on('slid.bs.carousel', (event) => {
+    console.log('slid: ', `
+      方向：${ event.direction }
+      从：${ event.from }
+      到：${ event.to }
+    `)
+  })
 
 })
