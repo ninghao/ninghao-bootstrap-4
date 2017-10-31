e6ead97a8eef42cf3d5be786cb88568e308b55f9
wanghao8080 <117663444@qq.com>
2017-10-31 14:28:08 +0800

    Nav：导航

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 $(document).ready(() => {
 
-  $('#modal-demo').on('show.bs.modal', function () {
-    console.log('show')
-  })
-  $('#modal-demo').on('shown.bs.modal', function () {
-    console.log('shown')
-  })
-  $('#modal-demo').on('hide.bs.modal', function () {
-    console.log('hide')
-  })
-  $('#modal-demo').on('hidden.bs.modal', function () {
-    console.log('hidden')
-  })
+  
 
 })
