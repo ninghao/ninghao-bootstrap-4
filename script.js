2309840e2fa611ebbf3a53e482c29f1870445e8f
wanghao8080 <117663444@qq.com>
2017-10-31 10:33:59 +0800

    Jumbotron：大屏幕

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 $(document).ready(() => {
 
-  $('#dropdown-demo').on('show.bs.dropdown', () => {
-    console.log('show')
-  })
-  $('#dropdown-demo').on('shown.bs.dropdown', () => {
-    console.log('shown')
-  })
-  $('#dropdown-demo').on('hide.bs.dropdown', () => {
-    console.log('hide')
-  })
-  $('#dropdown-demo').on('hidden.bs.dropdown', () => {
-    console.log('hidden')
-  })
+  
 
 })
