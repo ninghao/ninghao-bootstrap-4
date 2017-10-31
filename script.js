c83d6c45150ab7995f88ddd55cd6431609efcd95
wanghao8080 <117663444@qq.com>
2017-10-31 12:46:24 +0800

    List group：方法与事件

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(document).ready(() => {
 
-  
+  const list = $('#tab-demo .list-group a')
+  list.click(function (event) {
+    event.preventDefault()
+    $(this).tab('show')
+  })
+
+  list.on('show.bs.tab', function (event) {
+    console.log(`开始显示：${ event.target.getAttribute('href') }`)
+  })
+  list.on('shown.bs.tab', function (event) {
+    console.log(`完全显示：${ event.target.getAttribute('href') }`)
+  })
+  list.on('hide.bs.tab', function (event) {
+    console.log(`开始隐藏：${ event.target.getAttribute('href') }`)
+  })
+  list.on('hidden.bs.tab', function (event) {
+    console.log(`完全隐藏：${ event.target.getAttribute('href') }`)
+  })
 
 })
