a146bdc1c8fae8b6e3abedfeb5a19793327dbb43
wanghao8080 <117663444@qq.com>
2017-10-31 13:33:50 +0800

    Modal：对话框的选项，方法与事件

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(() => {
 
-  
+  $('#modal-demo').on('show.bs.modal', function () {
+    console.log('show')
+  })
+  $('#modal-demo').on('shown.bs.modal', function () {
+    console.log('shown')
+  })
+  $('#modal-demo').on('hide.bs.modal', function () {
+    console.log('hide')
+  })
+  $('#modal-demo').on('hidden.bs.modal', function () {
+    console.log('hidden')
+  })
 
 })
