5931a683cb0c9f7bd43d1a1284ddf290db0577ad
wanghao8080 <117663444@qq.com>
2017-11-02 13:10:50 +0800

    表单验证

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(() => {
 
-  
+  $('#form-demo').submit(function (event) {
+    const form = $(this)
+
+    if (form[0].checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.addClass('was-validated')
+  })
 
 })
